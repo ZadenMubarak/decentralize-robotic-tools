296e8aec4e2d420da8fb14c13580593ff9dcbb2b
ZadenMubarak <zaden.ngobeni@gmail.com>
2022-09-26 11:49:56 -0700

    states stated

--- tools/src/components/pack/droid.js
+++ tools/src/components/pack/droid.js
@@ -2,15 +2,21 @@ import React, {useState} from 'react'
 import './droid.css'
 
 
+const showData = () => {
+  return(
+    <h5>ibhanoyi</h5>
+  )
+}
+
 
 const Droid = () => {
   
-  // const [div_format, current_look] = useState(null)
-  // console.log(current_look)
+  const [show, setshow] = useState(false);
+
   return (
 
     <div>
-        <div className='side-card'>
+        <div className='side-card' onClick={() => setshow(!show)}>
           <br/><br/>
 
             <h1>humanoid pack</h1>
@@ -25,11 +31,28 @@ const Droid = () => {
             <br/><br/>
             <h3><li>Emotion ❤️ 😃</li></h3>
             <br/><br/>
-            <h3><li>Self destruct -- Default</li></h3>
+            <h3><li>Self destruction -- Default</li></h3>
+
         </div>
 
         <div className='midcard'>
+          {
+          show ? showData: 
+          <>
           <h2>Get pack</h2>
+          <div className='imgcard'>
+            <h2>Get Pack --------</h2>
+            <br/><br/><br/>
+            <h3>Buy package ---------</h3>
+            <br/><br/>
+            <h3>P.S packages bought are stored as NFT's on moralis ---------<br/>
+              incase anything happens to your device-----------
+            </h3>
+          
+          </div>
+          </>
+          }
+          {/* <h2>Get pack</h2>
           <div className='imgcard'>
             <h2>Get Pack</h2>
             <br/><br/><br/>
@@ -38,7 +61,9 @@ const Droid = () => {
             <h3>P.S packages bought are stored as NFT's on moralis<br/>
               incase anything happens to your device
             </h3>
-          </div>
+          
+          </div> */}
+          <h1>northy heitada</h1>
         </div>
 
         
